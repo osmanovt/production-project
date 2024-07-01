5f65e84882d43d58ffb110e4b74c11bee5bebee6
Tural <osmanov.tural2013@yandex.ru>
2024-07-01 18:09:21 +0300

    add config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,33 @@
+import { BuildOptions } from './types/config';
+import webpack from 'webpack';
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
+
+export function buildWebpackConfig(
+    options: BuildOptions
+): webpack.Configuration {
+    const { mode, paths, isDev } = options;
+    return {
+        mode: mode,
+        // entry: {
+        //   RANDOM: path.resolve(__dirname, 'src', 'index.tsx'),
+        // },
+        entry: paths.entry,
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            // обработка файлов отличимых от js
+            rules: buildLoaders(options),
+        },
+        // файлы для которых не нужно указывать расширение при import
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    };
+}
