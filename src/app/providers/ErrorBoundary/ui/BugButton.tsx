97f8ab350acfa6c245bde3c1d673eec6d51756f8
Tural <osmanov.tural2013@yandex.ru>
2024-07-06 21:56:12 +0300

    add ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,18 @@
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+
+// Компонент для тестирования
+export const BugButton = () => {
+    const { t } = useTranslation();
+    const [error, setError] = useState(false);
+    const onThrow = () => setError(true);
+
+    useEffect(() => {
+        if (error) {
+            throw new Error();
+        }
+    }, [error]);
+
+    return <Button onClick={onThrow}>{t('Ошибка')}</Button>;
+};
